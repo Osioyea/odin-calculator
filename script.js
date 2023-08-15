89b6f42e3b71cbdc74cfab4118712fe454f16617
Osioyea <131928742+Osioyea@users.noreply.github.com>
2023-08-15 14:47:55 -0500

    Cause button inputs to display on screen.

--- script.js
+++ script.js
@@ -1,9 +1,81 @@
-let firstInput = 9;
-let secondImput = 10;
-let operator = "+";
+let firstInput = "";
+let secondInput = "";
+let operator = "";
+
+const lastOperation = document.querySelector('.lastOperation');
+const answer = document.querySelector('.answer');
+const displayedOperation = document.querySelector('.displayedOperation');
+const clearAll = document.querySelector('#clearAll');
+const clearOne = document.querySelector('#clearOne');
+const decimal = document.querySelector('#decimal');
+const equals = document.querySelector('#equals');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+
+clearAll.addEventListener('click', () => {reset();});
+clearOne.addEventListener('click', () => {console.log("bruh");});
+decimal.addEventListener('click', () => {console.log("bruh");});
+equals.addEventListener('click', () => {updateAnswer(operate(firstInput, operator, secondInput))});
+
+numbers.forEach((item) => {
+  item.addEventListener('click', () => {editNumbers(item.innerText);});
+})
+
+operators.forEach((item) => {
+  item.addEventListener('click', () => {editOperator(item.innerText);});
+})
+
+function editNumbers(input){
+  if(operator == "")
+    firstInput += input;
+  else
+    secondInput += input;
+  updateDisplayedOperation();
+}
+
+function editOperator(input){
+  if(firstInput != "" && secondInput == "")
+    operator = input;
+  updateDisplayedOperation();
+}
+
+function updateLastOperation(){
+
+}
+
+function updateAnswer(input){
+
+}
+
+function updateDisplayedOperation(){
+  displayedOperation.textContent = firstInput + " " + operator + " " + secondInput;
+}
+
+function testDisplay(){
+  console.log(
+    "first input: " + firstInput,
+    "\nsecond input: " + secondInput,
+    "\noperator: " + operator
+  );
+}
+
+function reset(){
+  lastOperation.textContent = " ";
+  answer.textContent = "";
+  displayedOperation.textContent = "";
+  clearInputs();
+}
+
+function clearInputs(){
+  firstInput = "";
+  secondInput = "";
+  operator = "";
+}
 
 function operate(firstNumber, operation, secondNumber){
   let result = 0;
+  firstNumber = Number(firstNumber);
+  secondNumber = Number(secondNumber);
   switch(operation){
     case "+":
       result = add(firstNumber, secondNumber);
@@ -11,12 +83,18 @@ function operate(firstNumber, operation, secondNumber){
     case "-":
       result = subtract(firstNumber, secondNumber);
       break;
-    case "*":
+    case "x":
       result = multiply(firstNumber, secondNumber);
       break;
     case "/":
       result = divide(firstNumber, secondNumber);
       break;
+    case "%":
+      result = mod(firstNumber, secondNumber);
+      break
+    case "^":
+      result = power(firstNumber, secondNumber);
+      break
   }
   return result;
 }
@@ -39,5 +117,12 @@ function divide(a, b){
   }
   return a / b;
 }
+function mod(a, b){
+  return a % b;
+}
+function power(a, b){
+  return Math.pow(a,b);
+}
 
-console.log(operate(firstInput, operator, secondImput));
+console.log(operate(firstInput, operator, secondInput));
+console.log(typeof firstInput);
